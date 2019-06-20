84c289ec756a25c16090e30e7d43149a3c24b581
Daniel Preciado <danielpre30@outlook.com>
2019-06-19 23:12:44 -0500

    Estilos de la barra de navegacion
    Se cambio la renderizacion de la barra de navegacion a App.js para que solo se renderice una vez y no en cada pagina. Se agregaron las fuentes al proyecto en resources/OpenSans

--- src/pages/Employees.js
+++ src/pages/Employees.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import NavBar from '../components/NavBar'
 import CardContainer from '../containers/CardContainer'
 
 class Employees extends Component{
@@ -9,7 +8,6 @@ class Employees extends Component{
     render(){
         return (
             <div>
-                <NavBar/>
                 <h1>Employees</h1>
                 <CardContainer cardType="employees"/>
             </div>
--- src/pages/Prizes.js
+++ src/pages/Prizes.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import NavBar from '../components/NavBar'
 import CardContainer from '../containers/CardContainer'
 
 const Prizes = () => {
     return (
         <div>
-            <NavBar />
             <h1>Prizes</h1>
             <CardContainer cardType="prizes" />
         </div>
